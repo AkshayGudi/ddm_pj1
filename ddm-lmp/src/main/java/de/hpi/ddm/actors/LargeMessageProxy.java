a546609137d9821251ea185fc7455da1fac7168d
AkshayGudi <akshaygudi1993@gmail.com>
2019-11-14 14:53:25 +0100

    Added code to Chunk large data and transfer using Akka Streams

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,11 +1,29 @@
 package de.hpi.ddm.actors;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
+import akka.NotUsed;
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
+import akka.japi.function.Creator;
+import akka.serialization.Serialization;
+import akka.serialization.SerializationExtension;
+import akka.serialization.Serializers;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.OverflowStrategy;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.util.ByteString;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -41,6 +59,16 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		private ActorRef receiver;
 	}
 	
+	@Data @NoArgsConstructor @AllArgsConstructor
+	public static class CustomMessage implements Serializable {
+		private static final long serialVersionUID = 2237807743872319842L;
+		private byte[] byteData;
+		private ActorRef senderActor;
+		private ActorRef receiverActor;
+		private Integer serializerID;
+		private String manifest;
+	}
+	
 	/////////////////
 	// Actor State //
 	/////////////////
@@ -58,23 +86,113 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		return receiveBuilder()
 				.match(LargeMessage.class, this::handle)
 				.match(BytesMessage.class, this::handle)
+				.match(ByteString.class, this::handle)
+				
+				.matchEquals("ack", msg -> {
+					log().info("got value");
+				})
+				.matchEquals("init", msg -> {
+					log().info("Stream initialized");
+					sender().tell("ack", self());
+				}).matchEquals("done", completed -> {
+					log().info("Stream completed");
+				}).match(StreamFailure.class, failed -> {
+					log().error(failed.getCause(), "Stream failed!");
+				})
+				
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
 	}
 
+	
+
+	static class StreamFailure {
+		private final Throwable cause;
+
+		public StreamFailure(Throwable cause) {
+			this.cause = cause;
+		}
+
+		public Throwable getCause() {
+			return cause;
+		}
+	}
+	
 	private void handle(LargeMessage<?> message) {
+		
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
 		
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
-		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
-		receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
+		CompletionStage<ActorRef> resolveOne = receiverProxy.resolveOne(java.time.Duration.ofSeconds(30));
+		ActorRef finalReceiver = null;
+		try {
+			 finalReceiver = resolveOne.toCompletableFuture().get();
+		} catch (InterruptedException e) {
+			log().error("Error while getting actor");
+		} catch (ExecutionException e) {
+			log().error("Error while getting actor");
+		}
+		
+		Serialization serialization = SerializationExtension.get(getContext().getSystem());
+		byte[] serializedByteArray = serialization.serialize(message.getMessage()).get();
+		int serializerId = serialization.findSerializerFor(message.getMessage()).identifier();
+		String manifest = Serializers.manifestFor(serialization.findSerializerFor(message.getMessage()), message.getMessage());
+
+		CustomMessage serializedData = new CustomMessage(serializedByteArray, this.sender(), message.getReceiver(),
+				serializerId,
+				manifest);
+		
+		final Materializer materializer = ActorMaterializer.create(getContext().getSystem());
+		
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream oos;
+		try {
+		
+			oos = new ObjectOutputStream(bos);
+			oos.writeObject(serializedData);
+			oos.flush();
+			oos.close();
+			bos.close();
+		
+		} catch (IOException e) {
+			log().error("Error while serializing data");
+		}
+		
+		
+		
+		byte[] byteArrayData = bos.toByteArray();
+		ByteString fromArray = ByteString.fromArray(byteArrayData);
+		System.out.println(byteArrayData.length);
+		
+		Source<ByteString,NotUsed> wordSource = Source.single(fromArray);
+		Source<ByteString, NotUsed> chunkSource = wordSource.via(new Chunker(1000));
+				
+		Sink<ByteString, NotUsed> sink =
+		  Sink.<ByteString>actorRefWithAck(finalReceiver, "init", "ack", "done", ex ->
+		  new StreamFailure(ex));
+		 
+				
+		chunkSource.map(el -> el).runWith(sink, materializer);
+		
+//		receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
+//		receiverProxy.tell(serializedData, this.self());
+		
 	}
 
+	private void handle(ByteString message) {
+
+		log().info("Am in receiver");
+		Serialization serialization = SerializationExtension.get(getContext().getSystem());
+
+		/*
+		 * @SuppressWarnings("unused") Object deserializedObject = serialization
+		 * .deserialize(message.getByteData(), message.getSerializerID(),
+		 * message.getManifest()).get();
+		 */		
+		getSender().tell("ack", this.getSelf());		
+//		message.getReceiverActor().tell(deserializedObject, message.getSenderActor());
+	}
+	
 	private void handle(BytesMessage<?> message) {
 		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
 		message.getReceiver().tell(message.getBytes(), message.getSender());
